836b8c50110d31783a301bdf6c664f935029796e
Monika-feg <mala88@hotmail.comcd>
2025-10-21 09:48:40 +0200

    gjort en engel home sida

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Välkommen till KawaiiCanvas!! 🎀</h1>
+      <p>
+        Varning: Våra tavlor kan orsaka akut “awww”-syndrom, överdrivet leende
+        och behov av fler väggar 🐰💖
+      </p>
+    </div>
+  );
+}
+export default HomePage;
