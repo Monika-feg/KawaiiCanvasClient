c0deb4ef7965ef6a612e4556304afe2e7ac19d04
Monika-feg <mala88@hotmail.comcd>
2025-10-22 08:00:47 +0200

    har lagt till en footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import "../components/css/Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footer-container">
+          <p>© 2023 KawaiiCanvas. All rights reserved.</p>
+          <p>Made with ❤️ by KawaiiCanvas Team</p>
+          <p>Follow us on social media!</p>
+        </div>
+      </footer>
+    </>
+  );
+}
+export default Footer;
